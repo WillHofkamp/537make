ebf8ec3e203f596f306c07cbad2e458330a45f8b
gowni <47999529+gowni@users.noreply.github.com>
2020-11-05 10:06:37 -0600

    Update executeSpecificationGraph.c

--- executeSpecificationGraph.c
+++ executeSpecificationGraph.c
@@ -10,34 +10,31 @@
 
 
 #include "executeSpecificationGraph.h"
-int wasexec = 0;
+int executed = 0;
 
 //This method loops through each node and checks if they can be executed
-void checkNodes(GraphNode** order) {
+void checkNodes(GraphNode** flow) {
 	int execute = 1;
 	int i = 0;
-	// printGraph(order);
-	while (order[i] != NULL) {
-		// check if it's a target
-		// if its line number = -1, don't exec
-		if (order[i]->line < 0) {
+	while (flow[i] != NULL) {
+		// check if node is a target
+		if (flow[i]->line < 0) {
 			execute = 0;
 		}
-		// check if it's a file
-		if (access(order[i]->name, F_OK) == 0) {
-			// if so, check if it's up to date
-			if (!modificationCheck(order[i])) {
+		// check if node is a file
+		if (access(flow[i]->name, F_OK) == 0) {
+			if (!modificationCheck(flow[i])) {
 				execute = 0;
 			}
 		}
-		// great! you're good to go!
+		// execute node
 		if (execute) {
-			executeNodeProcess(order[i]);
+			executeNodeProcess(flow[i]);
 		}
 		i++;
 		execute = 1;
 	}
-	if (!wasexec) {
+	if (!executed) {
 		fprintf(stderr, "All files up-to-date\n");
 	}
 
@@ -62,8 +59,7 @@ void executeNodeProcess(GraphNode* node) {
 			fprintf(stderr, "Process could not be forked\n");
 			exit(0);
 		}
-               	else if(pid == 0){
-			// EXECUTE THE LINE
+            else if(pid == 0){
 			execvp(cmdList[0], cmdList);
 			exit(EXIT_FAILURE);
 		}
@@ -72,33 +68,34 @@ void executeNodeProcess(GraphNode* node) {
 			wait(&status);
 			if (WEXITSTATUS(status)) {
 				// didn't exit normally
-				fprintf(stderr,"%i: Invalid command \"", *line);
-				int y = 0;
-				while (cmdList[y] != NULL) {
-					fprintf(stderr, "%s ", cmdList[y]);
-					y++;
+				fprintf(stderr,"Invalid command \n");
+				int i = 0;
+				while (cmdList[i] != NULL) {
+					fprintf(stderr, "%s ", cmdList[i]);
+					i++;
 				}
 				fprintf(stderr, "\"\n");
 				exit(0);
 			}
 			else {
-				// manually print command
-				int x = 0;
-				while (cmdList[x] != NULL) {
-					fprintf(stderr, "%s ", cmdList[x]);
+				//print cmd
+				int j = 0;
+				while (cmdList[j] != NULL) {
+					fprintf(stderr, "%s ", cmdList[j]);
 				x++;
 				}
 				fprintf(stderr, "\n");
 			}
 		}	
-		//done executing one line
-		wasexec = 1;
+		//line executed
+		executed = 1;
 		(*line)++;
 
-		// free previous command list
-		for (int f = 0; f < CMD_SIZE; f++) {
-			// pay respects
-			free(cmdList[f]);
+		// free command list
+		int k = 0;
+		while(i<CMD_SIZE){
+			free(cmdList[k]);
+			i++;
 		}
 		free(cmdList);
 
@@ -125,16 +122,13 @@ int modificationCheck(GraphNode* node) {
 	time_t t_node = getFileModifiedTime(node->name);
 	time_t t_child;
 
-	// loop thru children
+	// loop throught the children
 	for (int i = 0; i < node->numchild; i++) {
-		//do thing
 		t_child = getFileModifiedTime(node->children[i]->name);
-		// if child was modified MORE SECONDS after the parent,
-		// then parent needs to be recompiled.
+		// recompile parent  if child was modified later than parent
 		if( difftime(t_node, t_child) <= 0 && t_child != 0) {
 			return 1;
 		}
 	}
-	// no need to compile
 	return 0;
 }
